863a34c94edac58e46e8d6d038b9a176acbddc43
ahmed0324 <ahmedraza032r@gmail.com>
2024-01-03 20:23:25 +0500

    temperature types

---
+++ ques5.dart
@@ -0,0 +1,17 @@
+void main(){
+    num temp = 20;
+  if(temp<=0){
+    print('freezing whether');
+  }else if(temp>0 && temp <=10){
+    print('Very Cold whether');
+   }
+  else if(temp>10 && temp <=20){
+    print(' Cold whether');
+   }
+  else if(temp>30 && temp <=40){
+    print('Hot whether');
+   }
+  else{
+    print('very Hot');
+  }
+}
