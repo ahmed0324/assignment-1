bb3d2d6d521a3f5c527e85655b055b9c3408f50b
ahmed0324 <ahmedraza032r@gmail.com>
2024-01-03 20:28:20 +0500

    vowel and consonants

---
+++ ques6.dart
@@ -0,0 +1,8 @@
+void main(){
+  var alphabet = 'e';
+  if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
+    print('$alphabet is a vowel.');
+  } else {
+    print('$alphabet is a consonant.');
+  }
+}
