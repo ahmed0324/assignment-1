e67aaa8c6f9b08529edbb0d68b02cfd3818f3135
ahmed0324 <ahmedraza032r@gmail.com>
2024-01-03 20:40:56 +0500

    Even and Odd

--- ques9.dart
+++ ques9.dart
@@ -1,3 +1,19 @@
 void main(){
-  
+    int number = 25; // Replace with the number you want to check
+
+  if (number % 2 == 0) {
+    // Number is even
+    if (number % 5 == 0) {
+      print("The number $number is even and divisible by 5.");
+    } else {
+      print("The number $number is even but not divisible by 5.");
+    }
+  } else {
+    // Number is odd
+    if (number % 7 == 0) {
+      print("The number $number is odd and divisible by 7.");
+    } else {
+      print("The number $number is odd but not divisible by 7.");
+    }
+  }
 }
