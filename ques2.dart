b365354a14099a0f80a1e74274513c34969db0d6
ahmed0324 <ahmedraza032r@gmail.com>
2024-01-03 20:00:49 +0500

    younger and older

---
+++ ques2.dart
@@ -0,0 +1,14 @@
+void main()
+{
+  int p1_age = 20;
+  int p2_age = 10;
+  if(p1_age>p2_age){
+    print('Person one is Older and Person two is younger');
+  }
+  else if(p2_age>p1_age){
+    print('Person two is Older and Person one is younger');
+  }
+  else{
+    print('both are of same age');
+  }
+}
